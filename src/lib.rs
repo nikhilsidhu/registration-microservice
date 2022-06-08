07da918e9fe9e9dd0f63efd67021d01d8e218dc8
sidhu80 <70008858+sidhu80@users.noreply.github.com>
2022-06-07 21:17:02 -0500

    created lib.rs & added dependencies

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod queue_actor;
+ 
+use actix::{Message, SystemRunner};
+use failure::Error;
+use futures::Future;
+use lapin::channel::{Channel, QueueDeclareOptions};
+use lapin::client::{Client, ConnectionOptions};
+use lapin::error::Error as LapinError;
+use lapin::queue::Queue;
+use lapin::types::FieldTable;
+use serde_derive::{Deserialize, Serialize};
+use tokio::net::TcpStream;
+
