5bcbf74e58230f5a50289f326a176de6cde78d08
sidhu80 <70008858+sidhu80@users.noreply.github.com>
2022-06-08 04:26:31 -0500

    added QueueHandler trait and main() (worker.rs)

--- src/worker.rs
+++ src/worker.rs
@@ -4,3 +4,44 @@ use log::debug;
 use qr_microservice::queue_actor::{QueueActor, QueueHandler, TaskId};
 use qr_microservice::{Request, Response, REQUESTS, RESPONSES};
 
+// Worker has no state
+struct WorkerHandler {}
+
+/*
+* [TRAIT] QueueHandler
+* 
+*/
+impl QueueHandler for WokerHandler {
+  type Incoming = QrRequest;
+  type Outgoing = QrResponse;
+
+  fn incoming(&self) -> &str {
+    REQUESTS
+  }
+  fn outgoing(&self) -> &str {
+    RESPONSES
+  }
+  fn handle(
+    &self,
+    _: &TaskId,
+    incoming: Self::Incoming,
+  ) -> Result<Option<Self::Outgoing>, Error> {
+    debug!("In: {:?}", incoming);
+    // TODO: call helper function (not yet implemented)
+    
+    debug!("Out: {:?}", outgoing);
+    Ok(Some(outgoing))
+  }
+}
+
+impl WorkerHandler {
+  // TODO: implement function called by handler
+}
+
+fn main() -> Result<(), Error> {
+  env_logger::init(); // for logging
+  let mut sys = System::new("registration-worker"); // starts a System instance
+  let _ = QueueActor::new(WorkerHandler {}, &mut sys)?; // Creates a QueueActor instance
+  let _ = sys.run(); // starts the system
+  Ok(())
+}
